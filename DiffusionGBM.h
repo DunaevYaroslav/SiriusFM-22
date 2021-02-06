78371b6854f0211875475749f0df485861e0d0f1
DunaevYaroslav <70894113+DunaevYaroslav@users.noreply.github.com>
2021-02-06 09:33:06 +0300

    first commit FM

---
+++ DiffusionGBM.h
@@ -0,0 +1,28 @@
+#include <cmath>
+#include <ctime>
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <thread>
+namespace SiriusFM{
+    class DiffusionGBM
+    {
+        private:
+            double const const_mu;
+            double const const_sigma;
+        public:
+            double mu(double St, double t) const{
+                return const_mu*St;
+            }
+            double sigma(double St, double t) const{
+                if(const_sigma<=0){return 0;}
+                return const_sigma*St;
+            }
+        DifffusionGBM(double const_m, double const_s)
+        :const_mu(const_m)
+         const_sigma(const_s)
+        {
+            if(const_sigma<=0){throw std::invalid_argument('Bad arguments');}
+        }
+    }
+}
